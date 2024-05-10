0a55b3fb1af3236ba36fb9903355427750050e2b
Eric Sheffield <ersheff@gmail.com>
2024-05-10 16:23:06 -0400

    asdf

--- api/echoFormData.js
+++ api/echoFormData.js
@@ -5,8 +5,10 @@ const xss = require("xss");
 module.exports = async (req, res) => {
   const formData = await text(req);
   const data = querystring.parse(formData);
+
   for (let key in data) {
     data[key] = xss(data[key]);
   }
+  
   res.end(JSON.stringify(data));
 };
