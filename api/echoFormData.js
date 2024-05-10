4fca9a3ec02da994491ed491e3d2dc7119c1f13b
Eric Sheffield <ersheff@gmail.com>
2024-05-10 16:15:57 -0400

    asdf

--- api/echoFormData.js
+++ api/echoFormData.js
@@ -1,12 +1,12 @@
 const { text } = require("micro");
 const querystring = require("querystring");
-const escapeHtml = require("escape-html");
+const xss = require("xss");
 
 module.exports = async (req, res) => {
   const formData = await text(req);
   const data = querystring.parse(formData);
   for (let key in data) {
-    data[key] = escapeHtml(data[key]);
+    data[key] = xss(data[key]);
   }
   res.end(JSON.stringify(data));
 };
