d383d6d1a79a4be11e447d676bf5a9101ed22235
Eric Sheffield <ersheff@gmail.com>
2024-05-10 15:24:36 -0400

    asdf

--- api/echoFormData.js
+++ api/echoFormData.js
@@ -1,10 +1,17 @@
 const { text } = require("micro");
 const querystring = require("querystring");
 
+const allowedOrigins = ["http://localhost:5500", "http://localhost:5501"];
+
 module.exports = async (req, res) => {
   const formData = await text(req);
   const data = querystring.parse(formData);
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.end(JSON.stringify(data));
