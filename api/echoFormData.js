50c8056bb54a1b88ae8e4469d4f931b0603ca945
Eric Sheffield <ersheff@gmail.com>
2024-05-10 15:53:10 -0400

    asdf

--- api/echoFormData.js
+++ api/echoFormData.js
@@ -5,8 +5,8 @@ const escapeHtml = require("escape-html");
 module.exports = async (req, res) => {
   const formData = await text(req);
   const data = querystring.parse(formData);
-  for (let key in data) {
-    data[key] = escapeHtml(data[key]);
-  }
+  // for (let key in data) {
+  //   data[key] = escapeHtml(data[key]);
+  // }
   res.end(JSON.stringify(data));
 };
