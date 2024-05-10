6a176953aa4602475f2a91da783ac4af65de23f6
Eric Sheffield <ersheff@gmail.com>
2024-05-10 15:46:34 -0400

    asdf

--- api/echoFormData.js
+++ api/echoFormData.js
@@ -3,15 +3,15 @@ const querystring = require("querystring");
 const escapeHtml = require("escape-html");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const formData = await text(req);
   const data = querystring.parse(formData);
 
   for (let key in data) {
     data[key] = escapeHtml(data[key]);
   }
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.end(JSON.stringify(data));
 };
